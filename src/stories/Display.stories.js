7dfa70cc8c14f1aa48c9cbc5cf5196cf8ed9a6c5
Mohammad-Haseeb <mohammad.haseeb310@gmail.com>
2021-02-10 01:47:26 +0500

    StoryBook

---
+++ src/stories/Display.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DisplayText from './../components/Display';
+
+export default{
+    title:"Text",
+    component:DisplayText
+}
+
+export const TextDisplayFirst=()=>{
+     return(
+    <DisplayText/>
+     );
+}
