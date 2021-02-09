8f90c6ee2d2066595da5d6b5261246cf6d239127
Mohammad-Haseeb <mohammad.haseeb310@gmail.com>
2021-02-10 02:35:20 +0500

    Update Display.js

--- src/components/Display.js
+++ src/components/Display.js
@@ -7,7 +7,7 @@ export default function DisplayText(props){
     let [state, setstate] = useState(0);
      return(
          <>
-         <h1 className={["container" ,props.colors ].join(" ")} >Hello</h1>
+         <h1 className={["container" ,props.colors ].join(" ")} >Hello 0</h1>
          <h1>Value : {state}</h1>
          <br/>
          <ButtonDisplay color="yellow" radius="10px" onClick={()=>{
